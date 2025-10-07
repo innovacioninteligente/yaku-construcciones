785b6c4904021ce7a0935a605f5240c1ee24a1b7
Andrés Gómez <innovacioninteligente.ia@gmail.com>
2025-10-07 14:57:26 +0000

    Hagamos un cambio. Para el Favicon usamos el anterior, para el logo usam

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,11 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
-  const logoUrl = "https://firebasestorage.googleapis.com/v0/b/local-digital-eye.firebasestorage.app/o/business%2Fyaku%2Ficon.png?alt=media&token=1803ca86-deff-4e44-84ea-20f1fae4cbf4";
+  const logoUrl = "https://firebasestorage.googleapis.com/v0/b/local-digital-eye.firebasestorage.app/o/business%2Fyaku%2FLogo-Largo.png?alt=media&token=e97c09a2-6af6-415b-9da9-f96cb3aefef2";
   
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-xl font-bold font-headline", className)}>
-      <Image src={logoUrl} alt="Yaku Construcciones Logo" width={24} height={24} className="h-6 w-6" />
-      <span>Yaku Construcciones</span>
+      <Image src={logoUrl} alt="Yaku Construcciones Logo" width={140} height={35} className="h-auto" />
     </Link>
   );
 }
